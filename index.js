7a28c8fbaa44b2779eb00b21714276b5ea7bcc84
jamescross91 <james_cross91@hotmail.com>
2017-05-25 12:58:34 +0100

    Pull a file from S3 and return

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
+
+const getParams = {
+    Bucket: 'packt-james-test-bucket', // your bucket name,
+    Key: 'packt-test-file.txt' // path to the object you're looking for
+}
+
+app.get('/', function (req, res) {
+  s3.getObject(getParams, function(err, data) {
+    // Handle any error and exit
+      if (err)
+        return err;
+
+      var objectData = data.Body.toString('utf-8'); 
+      res.send(objectData);// Use the encoding necessary
+    });
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
